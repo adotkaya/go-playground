1f66fe8a598cd28177189d8d70766a2395562681
adotkaya <adotkaya@gmail.com>
2026-01-06 17:44:26 +0300

    Refactor code structure on routes.go

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -18,8 +18,6 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	dynamic := alice.New(app.sessionManager.LoadAndSave)
-
 	/* 	If you’re not using the justinas/alice package to help manage your
 	middleware chains, then you’d need to use the http.HandlerFunc()
 	adapter to convert your handler functions like app.home to a
@@ -32,6 +30,7 @@ func (app *application) routes() http.Handler {
 	app.sessionManager.LoadAndSave(http.HandlerFunc(app.snippetView)))
 	*/
 
+	dynamic := alice.New(app.sessionManager.LoadAndSave)
 	router.Handler(http.MethodGet, "/", dynamic.ThenFunc(app.home))
 	router.Handler(http.MethodGet, "/snippet/view/:id", dynamic.ThenFunc(app.snippetView))
 	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
@@ -39,12 +38,15 @@ func (app *application) routes() http.Handler {
 	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
 	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
 
+	/*router.Handler(http.MethodPost, "/snippet/create", app.sessionManager.LoadAndSave(app.requireAuthentication(http.HandlerFunc(app.snippetCreate))))
+	 * Without justinas/alice package
+	 */
+
 	protected := dynamic.Append(app.requireAuthentication)
 	router.Handler(http.MethodGet, "/snippet/create", protected.ThenFunc(app.snippetCreate))
 	router.Handler(http.MethodPost, "/snippet/create", protected.ThenFunc(app.snippetCreatePost))
 	router.Handler(http.MethodPost, "/user/logout", protected.ThenFunc(app.userLogoutPost))
 
 	standart := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
-
 	return standart.Then(router)
 }
